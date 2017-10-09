3ed205aaec0a3ad09f4e7e5ed800427a5f931eeb
Whotakesmyname <19500528+Whotakesmyname@users.noreply.github.com>
2017-10-10 01:23:52 +0800

    [refactor&fix&feature]Personal init commit
    Fixed some bugs, finished some todos and refactored the update part.

--- eprogress/eprogress.py
+++ eprogress/eprogress.py
@@ -1,16 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 """
-Created on 2017/7/21
+This is originally a package called eprogress which can be installed by 'pip install eprogress'.
+However there are too many bugs in it that my monkey patches almost cover the whole code.
+So I forked it here for the convenience of my repairing.
 
+The original package is under Apache Licence 2.0. For more detail see https://github.com/homgwu/eprogress.
+
+Harrisson Chen<chenhao11@xioami.com> @ 09/13/17
 """
-__author__ = 'HomgWu'
 
-import sys, re, abc, threading
+import abc
+import re
+import sys
+import threading
 
 CLEAR_TO_END = "\033[K"
-UP_ONE_LINE = "\033[F"
+UP_ONE_LINE = "\033[1A"
 
 
 class ProgressBar(object, metaclass=abc.ABCMeta):
@@ -41,6 +45,7 @@ class CircleProgress(ProgressBar):
         """
         super(CircleProgress, self).__init__(width=width, title=title)
         self._current_char = ''
+        self._status = 0
 
     def update(self, progress=0):
         """
@@ -48,37 +53,43 @@ class CircleProgress(ProgressBar):
         """
         with self.lock:
             if progress > 0:
-                self._current_char = self._get_next_circle_char(self._current_char)
+                self._status = 1
+            elif progress < 0:
+                self._status = 0
+            if self._status > 0:
+                self._get_next_circle_char()
             sys.stdout.write('\r' + CLEAR_TO_END)
             sys.stdout.write("\r%s:[%s]" % (self.title, self._current_char))
             # sys.stdout.flush()
 
-    def _get_next_circle_char(self, current_char):
-        if current_char == '':
-            current_char = '-'
-        elif current_char == '-':
-            current_char = '\\'
-        elif current_char == '\\':
-            current_char = '|'
-        elif current_char == '|':
-            current_char = '/'
-        elif current_char == '/':
-            current_char = '-'
-        return current_char
+    def _get_next_circle_char(self):
+        if self._current_char == '':
+            self._current_char = '-'
+        elif self._current_char == '-':
+            self._current_char = '\\'
+        elif self._current_char == '\\':
+            self._current_char = '|'
+        elif self._current_char == '|':
+            self._current_char = '/'
+        elif self._current_char == '/':
+            self._current_char = '-'
+        return self._current_char
 
 
 class LineProgress(ProgressBar):
-    def __init__(self, total=100, symbol='#', width=25, title=''):
+    def __init__(self, total=100, symbol='#', width=25, title='', is_2charswide=False):
         """
          @param total : 进度总数
          @param symbol : 进度条符号
          @param width : 进度条展示的长度
          @param title : 进度条前面展示的文字
+         @param is_2charswide : 占位符是否2字符宽，适应Windows环境下某些占位符2字符宽的显示问题
         """
         super(LineProgress, self).__init__(width=width, title=title)
         self.total = total
         self.symbol = symbol
         self._current_progress = 0
+        self.platform_adjustment = 2 if is_2charswide else 1
 
     def update(self, progress=0):
         """
@@ -88,9 +99,9 @@ class LineProgress(ProgressBar):
             if progress > 0:
                 self._current_progress = progress
             sys.stdout.write('\r' + CLEAR_TO_END)
-            hashes = '#' * int(self._current_progress / self.total * self.width)
-            spaces = ' ' * (self.width - len(hashes))
-            sys.stdout.write("\r%s:[%s] %d%%" % (self.title, hashes + spaces, self._current_progress))
+            hashes = self.symbol * int(self._current_progress / self.total * self.width)
+            spaces = ' ' * (self.width - len(hashes)) * self.platform_adjustment
+            sys.stdout.write("\r%s:[%s] %d%%" % (self.title, hashes + spaces, int(self._current_progress / self.total * 100)))
             # sys.stdout.flush()
 
 
@@ -125,10 +136,11 @@ class MultiProgressManager(object):
                 return
             delta_line = len(self._progress_dict)
             sys.stdout.write(UP_ONE_LINE * delta_line if delta_line > 0 else '')
-            for tmp_key in self._progress_dict.keys():
-                progress_bar = self._progress_dict.get(tmp_key)
-                tmp_progress = 0
-                if key == tmp_key:
-                    tmp_progress = progress
-                progress_bar.update(tmp_progress)
+            bars = list(self._progress_dict.items())
+            bars.sort(key=lambda x: x[1].index)
+            for inner_key, bar in bars:
+                if inner_key == key:
+                    bar.update(progress)
+                else:
+                    bar.update(0)
                 sys.stdout.write('\n')
